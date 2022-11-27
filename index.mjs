9d18f28f85a611b63bd1f4e41bb44753cd433d27
Darlan Alves <me@darlanalv.es>
2022-11-27 15:53:57 +0100

    create index.mjs

---
+++ index.mjs
@@ -0,0 +1,27 @@
+import { request } from 'https';
+import { readFileSync, writeFileSync } from 'fs';  
+import { spawnSync as sh } from 'child_process';
+
+const key = readFileSync('./key').toString('utf8')
+const shell = sh('which', ['sh']).stdout.toString('utf8').trim();
+
+console.log('Using', shell);
+
+function run() {
+  const remote = request('https://ghar.homebots.io/in', { method: 'POST' });
+  
+  remote.on('response', response => {
+    const chunks = [];
+    response.on('data'), c => chunks.push(c));
+    response.on('end', () => {
+      const buffer = Buffer.concat(chunks);
+      writeFileSync('./exec.sh', buffer.toString('utf8'));
+      const {stdout, stderr} = sh(shell, ['./exec.sh']);
+      const out = request('https://ghar.homebots.io/out', { method: 'POST' });
+      out.write(JSON.stringify({stdout, stderr}));
+      out.end();
+    });
+  });
+  remote.write(key);
+  remote.end();
+}
