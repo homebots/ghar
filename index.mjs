89f02288bdf0238c6183a43a0240f3a321be68b5
Darlan Alves <me@darlanalv.es>
2022-11-27 15:57:39 +0100

    chore: fix typo

--- index.mjs
+++ index.mjs
@@ -1,24 +1,25 @@
 import { request } from 'https';
-import { readFileSync, writeFileSync } from 'fs';  
+import { readFileSync, writeFileSync } from 'fs';
 import { spawnSync as sh } from 'child_process';
 
-const key = readFileSync('./key').toString('utf8')
+const key = readFileSync('./key').toString('utf8');
 const shell = sh('which', ['sh']).stdout.toString('utf8').trim();
+const empty = Buffer.concat([]);
 
 console.log('Using', shell);
 
 function run() {
   const remote = request('https://ghar.homebots.io/in', { method: 'POST' });
-  
-  remote.on('response', response => {
+
+  remote.on('response', (response) => {
     const chunks = [];
-    response.on('data'), c => chunks.push(c));
+    response.on('data', (c) => chunks.push(c));
     response.on('end', () => {
       const buffer = Buffer.concat(chunks);
       writeFileSync('./exec.sh', buffer.toString('utf8'));
-      const {stdout, stderr} = sh(shell, ['./exec.sh']);
+      const { stdout, stderr } = sh(shell, ['./exec.sh']);
       const out = request('https://ghar.homebots.io/out', { method: 'POST' });
-      out.write(JSON.stringify({stdout, stderr}));
+      out.write(JSON.stringify({ stdout: (stdout || empty).toString(), stderr: (stderr || empty).toString() }));
       out.end();
     });
   });
